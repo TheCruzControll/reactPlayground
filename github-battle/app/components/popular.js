afd56d22354f1d37954f9b726bf96040592215f2
Niko Cruz <francisnikocruz@gmail.com>
2019-09-21 14:40:12 -0700

    added more components

--- github-battle/app/components/popular.js
+++ github-battle/app/components/popular.js
@@ -1,20 +1,55 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default class Popular extends React.Component {
-    render() {
+function LanguagesNav({selected, onUpdateLanguage}){
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
-        const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
-
-        return(
-            <ul className='flex-center'>
-                {languages.map((language) => (
-                    <li key={language}>
-                        <button className='btn-clear nav-link'>
+    return(
+        <ul className='flex-center'>
+            {languages.map((language) => (
+                <li key={language}>
+                    <button 
+                        className='btn-clear nav-link'
+                        style = {language === selected ? {color: 'pink'} : null}
+                            onClick={() => onUpdateLanguage(language)}>
                             {language}
                         </button>
                     </li>
                 ))}
             </ul>
+        )
+}
+
+LanguagesNav.propTypes = {
+    selected: PropTypes.string.isRequired,
+    onUpdateLanguage: PropTypes.func.isRequired
+}
+
+export default class Popular extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            selectedLanguage: 'All'
+        }
+        this.updateLanguage = this.updateLanguage.bind(this)
+    }
+
+    updateLanguage(selectedLanguage){
+        this.setState({
+            selectedLanguage
+        })
+    }
+    render() {
+        const{selectedLanguage} = this.state
+
+        return(
+            <React.Fragment>
+                <LanguagesNav
+                    selected = {selectedLanguage}
+                    onUpdateLanguage = {this.updateLanguage}
+                />
+            </React.Fragment>
         )
     }
 }
